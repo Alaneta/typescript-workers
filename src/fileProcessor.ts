f96ad511568c892a8df6ddd1b69ab170719b3f69
Alan Camussi <acamussi@aper.com>
2024-06-09 14:38:56 -0300

    Add type to fileProcessor.ts

--- src/fileProcessor.ts
+++ src/fileProcessor.ts
@@ -1,7 +1,7 @@
 import { FileService } from './services'
 import { FileProcessResponseDTO } from './dtos/FileProcessResponseDTO'
 
-async function processFiles() {
+async function processFiles(): Promise<void> {
   try {
     const results: FileProcessResponseDTO = await FileService.process('countWords')
     console.log(results)
